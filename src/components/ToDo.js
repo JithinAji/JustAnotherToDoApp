6ab7c214203c486125a91e7f048dae62789fe744
Jithin Aji <jithinaji@Jithins-MacBook-Air.local>
2025-06-16 00:44:02 +0530

    add double click to edit feature

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,22 +1,58 @@
+import { useState } from "react";
 import "./ToDo.css";
 
 function ToDo({ content, isDone, id, markAsDone }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editableContent, setEditableContent] = useState(content);
+
   const handleDone = () => {
     markAsDone(id);
+  };
+
+  const handleDoubleClick = () => {
+    if (!isDone) {
+      setIsEditing(true);
+    }
+  };
+
+  const handleChange = (e) => {
+    setEditableContent(e.target.value);
+  };
+
+  const handleBlur = () => {
+    setIsEditing(false);
+    // Optional: Trigger a callback to save this change
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      setIsEditing(false);
+      // Optional: Save on Enter
+    }
   };
 
   return (
     <div className="todo">
       {!isDone ? (
-        <span>{content}</span>
+        isEditing ? (
+          <input
+            type="text"
+            value={editableContent}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            onKeyDown={handleKeyDown}
+            autoFocus
+            className="todo-input"
+          />
+        ) : (
+          <span onDoubleClick={handleDoubleClick}>{editableContent}</span>
+        )
       ) : (
         <span>
-          <del>{content}</del>
+          <del>{editableContent}</del>
         </span>
       )}
-      <button isDone={isDone} onClick={handleDone}>
-        {isDone ? "Undo" : "Mark as Done"}
-      </button>
+      <button onClick={handleDone}>{isDone ? "Undo" : "Mark as Done"}</button>
     </div>
   );
 }
